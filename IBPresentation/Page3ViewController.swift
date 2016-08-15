6d9b337b721664c984979f7bfd382c61752b04cf
Jake Lin <JakeLinAu@gmail.com>
2016-08-15 17:55:11 +1000

    Load website for page 3.

---
+++ IBPresentation/Page3ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Page3ViewController.swift
+//  IBPresentation
+//
+//  Created by Jake Lin on 8/12/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class Page3ViewController: UIViewController {
+  
+  @IBOutlet var webView: UIWebView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let url = NSURL(string: "http://design.lc/designer-news-statistics-2015/")!
+    webView.loadRequest(NSURLRequest(URL: url))
+  }
+}
